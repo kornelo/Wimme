9192b263983637bdca5821d0608294f9cfdfbd2f
l-wieczorek <mlody@MacBook-Pro-Mlody.local>
2017-11-11 11:43:38 +0100

    pods+conf

---
+++ Wimme/Pods/Target Support Files/Pods-Wimme/Pods-Wimme-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wimme : NSObject
+@end
+@implementation PodsDummy_Pods_Wimme
+@end
---
+++ Wimme/Pods/Target Support Files/Pods-WimmeTests/Pods-WimmeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WimmeTests : NSObject
+@end
+@implementation PodsDummy_Pods_WimmeTests
+@end

5eb9560c0516ef83cef5d658cd0d52d2505f0dce
Młody-MBP <lukasz.wieczorek@aol.com>
2017-11-11 16:14:00 +0100

    Database changes

--- Wimme/Wimme/Classes/EventsDatabase.swift
+++ Wimme/Wimme/Classes/EventsDatabase.swift
@@ -20,6 +20,12 @@ class EventsDatabase {
             "date": newEvent.date,
             "place": newEvent.place,
             "title": newEvent.title,
+            "description": newEvent.description,
+            "xEventLocalization": newEvent.eventLocalization.xVariable,
+            "yEventLocalization": newEvent.eventLocalization.yVariable,
+            "zEventLocalization": newEvent.eventLocalization.zVariable,
+            "pictureUrl": newEvent.pictureUrl
+            
             ] as [String : Any]
         refEventList.setValue(newEventData)
     }
